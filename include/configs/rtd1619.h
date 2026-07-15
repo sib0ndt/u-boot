c28d0c8b0adfbeaf81b341367a3bd94db5dd7d2c
sib0ndt <isibondt@gmail.com>
2026-07-15 15:15:52 +0700

    rtd1619: enable 6 cores support using spin-table method

--- include/configs/rtd1619.h
+++ include/configs/rtd1619.h
@@ -6,4 +6,18 @@
 #define CFG_SYS_INIT_SP_ADDR		0x07f00000
 #define CFG_SYS_BOOTM_LEN		0x04000000
 
+/* Extlinux/Distro boot support - USB only (device boots from USB) */
+#define BOOT_TARGET_DEVICES(func) \
+	func(USB, usb, 0)
+
+#include <config_distro_bootcmd.h>
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	"kernel_addr_r=0x08000000\0" \
+	"fdt_addr_r=0x0a000000\0" \
+	"scriptaddr=0x09000000\0" \
+	"ramdisk_addr_r=0x0b000000\0" \
+	"fdtfile=realtek/rtd1619-x1-prime-c.dtb\0" \
+	BOOTENV
+
 #endif
